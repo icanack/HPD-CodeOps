b7a0fb7b96dd5e7ebcf40173fbda08b1f4d650c8
icanack <icanack@gmail.com>
2018-04-29 12:30:18 -0300

    terceira aula de python

--- python/aula_python/docker-cli.py
+++ python/aula_python/docker-cli.py
@@ -1,11 +1,34 @@
 import docker
 import argparse
+from datetime import datetime
 
-client = docker.from_env()
-client.containers.run("nginx", detach=True)
-client.containers.list()
+def logando(mensagem, e, logfile='docker-cli.log'):
+	data_atual = datetime.now().strtime('%d%m%Y %H:%M')
+	with open('docker-cli.log', 'a') as log:
+		texto = "%s \t %s \t %s \n" % (data_atual, mensagem, str(e))
+		log.write(texto)
 
-def container():
+def criar_containers(args):
+	try:
+		client = docker.from_env()
+		executando = client.containers.run(args.imagem, args.comando, detach=True)
+		return(executando)
+	except docker.errors.ImageNotFound as e:
+		logando('Erro: Essa imagem nao existe!', e)
+	except docker.errors.NotFound as e:
+		logando('Erro: Esse comando não existe!', e)
+	except Exception as e:
+		logando('Erro: Favor verificar  comando executado', e)
+	finally:
+		print('Comando executado!')
+
+
+
+#client = docker.from_env()
+#client.containers.run("nginx", detach=True)
+#client.containers.list()
+
+def listar(args):
 	"""Listando os container e suas respectivas imagens"""
 	get_all = client.containers.list()
 	for cada_container in get_all:
@@ -18,8 +41,13 @@ subparser = parser.add_subparsers()
 listar_opt = subparser.add_parser('listar')
 listar_opt.set_defaults(func=listar)
 
-cmd = parser.parse.args()
+criar_opt = subparser.add_parser('criar')
+criar_opt.add_argument('--imagem', required=True)
+criar_opt.add_argument('--comando', required=False)
+criar_opt.set_defaults(func=criar_containers)
 
+
+cmd = parser.parse_args()
 cmd.func(cmd)
 
 #container()
